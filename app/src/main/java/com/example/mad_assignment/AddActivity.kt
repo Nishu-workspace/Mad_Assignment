0c3bfa8a9248dbd0fdc3f4a2c57abb6d33690522
Flare <82433193+Flare-workspace@users.noreply.github.com>
2024-10-09 20:33:31 +0530

    Improvements in Fonts: Added Downloadable fonts

--- app/src/main/java/com/example/mad_assignment/AddActivity.kt
+++ app/src/main/java/com/example/mad_assignment/AddActivity.kt
@@ -1,38 +1,20 @@
 package com.example.mad_assignment
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var addition:Button
-    lateinit var multiplication:Button
-    lateinit var substraction:Button
-
+class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        addition = findViewById<Button>(R.id.addButton)
-        multiplication = findViewById<Button>(R.id.mulButton)
-        substraction = findViewById(R.id.subButton)
-
-        addition.setOnClickListener{
-            val intent = Intent(this,AddActivity::class.java )
-            startActivity(intent)
-
-        }
-
     }
 }
